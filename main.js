d5802505fe9c2ecbccfc6371233ad99af4a798c6
Lucas Satelles <142444442+satelleslusca@users.noreply.github.com>
2024-10-07 10:37:43 -0300

    dsa

---
+++ main.js
@@ -0,0 +1,9 @@
+function criaCartao (categoria, pergunta, resposta){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    
+    `
+}
+    
