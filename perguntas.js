b91c3ac5fd078bb8f65401504170439a6e28f4ef
Lucas Satelles <142444442+satelleslusca@users.noreply.github.com>
2024-10-07 10:25:00 -0300

    jh

--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,25 @@
 criaCartao(
     'História - Getúlio Vargas',
-    'Qual foi o principal objetivo da Revolução de 1930 liderada por Getúlio
-    Vargas?',
+    'Qual foi o principal objetivo da Revolução de 1930 liderada por Getúlio Vargas?',
     'Derrubar o governo oligárquico e instaurar um governo centralizado no Brasil.'
     );
     
     criaCartao(
     'História - Getúlio Vargas',
     'O que foi o Estado Novo, implementado por Getúlio Vargas?',
-    'Foi uma ditadura instaurada em 1937, onde Vargas governou com amplos
-    poderes até 1945.'
+    'Foi uma ditadura instaurada em 1937, onde Vargas governou com amplos poderes ate 1945.'
     );
     
     criaCartao(
     'História - Getúlio Vargas',
     'Como Getúlio Vargas incentivou o desenvolvimento industrial no Brasil?',
-    'Criou empresas estatais como a Companhia Siderúrgica Nacional e
-    Petrobrás.'
+    'Criou empresas estatais como a Companhia Siderúrgica Nacional e Petrobrás.'
     );
     
     criaCartao(
     'História - Getúlio Vargas',
-    'Quais foram os principais direitos trabalhistas implementados durante o
-    governo Vargas?',
-    'Salário mínimo, jornada de trabalho de 8 horas, férias remuneradas, entre
-    outros.'
+    'Quais foram os principais direitos trabalhistas implementados durante o governo Vargas?',
+    'Salário mínimo, jornada de trabalho de 8 horas, ferias remuneradas, entre outros.'
     );
     
     criaCartao(
@@ -37,22 +32,19 @@ criaCartao(
     criaCartao(
     'Biologia - Gregor Mendel',
     'Quem foi Gregor Mendel?',
-    'Cientista conhecido como o &quot;pai da genética&quot; por seus experimentos com
-    ervilhas.'
+    'Cientista conhecido como o &quot;pai da genetica&quot; por seus experimentos com ervilhas.'
     );
     
     criaCartao(
     'Biologia - Gregor Mendel',
     'Qual foi a principal contribuição de Gregor Mendel para a biologia?',
-    'Ele descobriu as leis da hereditariedade, que explicam como os genes são
-    transmitidos de pais para filhos.'
+    'Ele descobriu as leis da hereditariedade, que explicam como os genes são transmitidos de pais para filhos.'
     );
     
     criaCartao(
     'Biologia - Gregor Mendel',
     'O que são os genes dominantes e recessivos segundo as Leis de Mendel?',
-    'Genes dominantes se expressam mesmo quando em heterozigose, enquanto
-    os recessivos só se expressam em homozigose.'
+    'Genes dominantes se expressam mesmo quando em heterozigose, enquanto os recessivos só se expressam em homozigose.'
     );
     
     criaCartao(
@@ -62,76 +54,68 @@ criaCartao(
     );
     
     criaCartao(
-    'Biologia - Genética',
-    'O que é genótipo e fenótipo?',
-    'Genótipo é a constituição genética de um organismo; fenótipo é a
-    manifestação física dessas características.'
+    'Biologia - Genetica',
+    'O que e genótipo e fenótipo?',
+    'Genótipo e a constituição genetica de um organismo; fenótipo e a  manifestação física dessas características.'
     );
     
     criaCartao(
-    'Biologia - Genética',
-    'O que é o cruzamento monohíbrido?',
-    'É um cruzamento entre indivíduos que diferem em apenas um caráter
-    genético.'
+    'Biologia - Genetica',
+    'O que e o cruzamento monohíbrido?',
+    'e um cruzamento entre indivíduos que diferem em apenas um caráter genetico.'
     );
     
     criaCartao(
-    'Biologia - Genética',
-    'Qual é a importância do experimento de Mendel para a genética moderna?',
-    'Estabeleceu as bases para o entendimento da hereditariedade e a
-    transmissão de características.'
+    'Biologia - Genetica',
+    'Qual e a importância do experimento de Mendel para a genetica moderna?',
+    'Estabeleceu as bases para o entendimento da hereditariedade e a transmissão de características.'
     );
     
     criaCartao(
     'Matemática - Seno, Cosseno e Tangente',
-    'O que é o seno de um ângulo em um triângulo retângulo?',
-    'É a razão entre o cateto oposto ao ângulo e a hipotenusa.'
+    'O que e o seno de um ângulo em um triângulo retângulo?',
+    'e a razão entre o cateto oposto ao ângulo e a hipotenusa.'
     );
     
     criaCartao(
     'Matemática - Seno, Cosseno e Tangente',
     
     'Como se calcula o cosseno de um ângulo em um triângulo retângulo?',
-    'É a razão entre o cateto adjacente ao ângulo e a hipotenusa.'
+    'e a razão entre o cateto adjacente ao ângulo e a hipotenusa.'
     );
     
     criaCartao(
     'Matemática - Seno, Cosseno e Tangente',
-    'O que é a tangente de um ângulo?',
-    'É a razão entre o cateto oposto e o cateto adjacente de um triângulo
-    retângulo.'
+    'O que e a tangente de um ângulo?',
+    'e a razão entre o cateto oposto e o cateto adjacente de um triângulo retângulo.'
     );
     
     criaCartao(
     'Matemática - Seno, Cosseno e Tangente',
-    'Qual é a relação fundamental entre seno, cosseno e tangente em um
-    triângulo retângulo?',
-    'A tangente é o resultado da divisão entre o seno e o cosseno de um ângulo.'
+    'Qual e a relação fundamental entre seno, cosseno e tangente em um triângulo retângulo?',
+    'A tangente e o resultado da divisão entre o seno e o cosseno de um angulo.'
     );
     
     criaCartao(
     'Matemática - Seno, Cosseno e Tangente',
-    'O que é a identidade trigonométrica fundamental?',
+    'O que e a identidade trigonometrica fundamental?',
     'Seno²(θ) + Cosseno²(θ) = 1, para qualquer ângulo θ.'
     );
     
     criaCartao(
     'Português - Romantismo',
     'O que caracteriza o Romantismo na literatura brasileira?',
-    'Uma exaltação dos sentimentos, do nacionalismo e da natureza, com
-    enfoque no individualismo.'
+    'Uma exaltação dos sentimentos, do nacionalismo e da natureza, com enfoque no individualismo.'
     );
     
     criaCartao(
     'Português - Romantismo',
     'Quais são os principais autores do Romantismo no Brasil?',
-    'José de Alencar, Gonçalves Dias e Álvares de Azevedo são alguns dos
-    principais autores.'
+    'Jose de Alencar, Gonçalves Dias e Álvares de Azevedo são alguns dos principais autores.'
     );
     
     criaCartao(
     'Português - Romantismo',
     'Qual foi o contexto histórico do surgimento do Romantismo no Brasil?',
-    'Surgiu no século XIX, em meio ao processo de independência do Brasil e à
-    busca por uma identidade nacional.'
+    'Surgiu no seculo XIX, em meio ao processo de independência do Brasil e à busca por uma identidade nacional.'
     );
